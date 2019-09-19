6b4a69b6ead4c43f1105bbd6d197d6a9c6a174b2
mekunuware <55546493+mekunuware@users.noreply.github.com>
2019-09-19 18:34:53 +0100

    Update gae-clean-imports.py

--- devscripts/gae-clean-imports.py
+++ devscripts/gae-clean-imports.py
@@ -2,11 +2,11 @@ from __future__ import unicode_literals
 
 import sys
 import re
-from codecs import open
+import io
 
 filename = sys.argv[1]
 
-with open(filename, 'rt', encoding='utf-8') as f:
+with io.open(filename, 'rt', encoding='utf-8') as f:
     lines = list(f.readlines())
 
 # This modules are not availabe in GAE, we don't use any of the functions
@@ -17,5 +17,5 @@ new_lines = (re.sub(
     r'()# Removed \1 import',
     l) for l in lines)
 
-with open(filename, 'wt', encoding='utf-8') as f:
+with io.open(filename, 'wt', encoding='utf-8') as f:
     f.writelines(new_lines)
